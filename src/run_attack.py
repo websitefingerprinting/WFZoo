cd2649b4fdce907ca3c9fdd70084867cdae14ac9
Jiajun GONG <jgongac@connect.ust.hk>
2024-01-12 15:28:07 +0800

    update argments

--- src/run_attack.py
+++ src/run_attack.py
@@ -39,6 +39,8 @@ def parse_arguments():
     # GPU
     parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+    parser.add_argument('--devices', type=str, default='0,1,2,3,4,5,6,7', help='device ids of multiple gpus')
     parser.add_argument('--amp', action='store_true', default=False, help='use mixed precision training')
     args = parser.parse_args()
     return args
